b7f951f02fec174c4f824e5279c862575919d06b
LucaBourke7 <A00031454@myTUDublin.ie>
2026-03-31 12:51:13 +0100

    Connect controller to instrument

--- main.gd
+++ main.gd
@@ -1,1 +1,13 @@
 extends Node2D
+
+var instrument
+var controller
+
+func _ready() -> void:
+	await get_tree().process_frame
+	instrument = get_node_or_null("Instrument")
+	controller = get_node_or_null("Controller")
+
+	if controller and instrument:
+		controller.note_on.connect(instrument.play_note)
+		controller.note_off.connect(instrument.stop_note)
